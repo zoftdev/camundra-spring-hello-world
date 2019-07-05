207bc219531ee0bf9a3ed4d00a58d263c47a8839
Thisadee Preeyawongsakul <thisadee_pre@truecorp.co.th>
2019-07-06 06:22:17 +0700

    hello

---
+++ src/main/java/com/hlex/lab/camundasrpringhelloworld/CamundaSrpringHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.hlex.lab.camundasrpringhelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaSrpringHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamundaSrpringHelloWorldApplication.class, args);
+	}
+
+}
